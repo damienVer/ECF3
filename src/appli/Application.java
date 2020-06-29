40fa81e3ab75a2ce2d5b3b4d23a8991a571757d6
damien <damien.verschaere@gmail.com>
2020-06-29 10:42:25 +0200

    essai de C12

--- src/appli/Application.java
+++ src/appli/Application.java
@@ -3,7 +3,7 @@ package appli;
 public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("hello world !!!!!!");
+		System.out.println("nom de zeu !!!!!! s");
 
 	}
 
